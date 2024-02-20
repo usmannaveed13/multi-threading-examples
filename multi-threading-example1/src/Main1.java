2fab9a7277672a188e36f09a5cbb5d2bd12dd967
Usman Naveed <usmna.naveed@2iqresearch.com>
2024-02-20 15:22:45 +0500

    creating initial project for thread

---
+++ multi-threading-example1/src/Main1.java
@@ -0,0 +1,23 @@
+package src;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //code write here that need to run
+                System.out.println("We are now in thread "+ Thread.currentThread().getName());
+                System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
+            }
+        });
+
+        thread.setName("New Worker Thread");
+
+        thread.setPriority(Thread.MAX_PRIORITY);
+
+        System.out.println("We are in thread: " + Thread.currentThread().getName()+ " before starting a new thread");
+        thread.start();
+        System.out.println("We are in thread: " + Thread.currentThread().getName()+ " after starting a new thread");
+    }
+}
